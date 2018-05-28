1e8d2a62f45f7868f9461a616cc76bdbf6677a8d
Tiago Amaro <tiago.amaro@theconversation.com>
2018-05-28 13:35:06 -0300

    Use singleton to instantiate the MiniProxy::Server

--- spec/support/proxy/mini_proxy.rb
+++ spec/support/proxy/mini_proxy.rb
@@ -1,3 +1,4 @@
+require "singleton"
 require "webrick/httpproxy"
 
 module MiniProxy
@@ -44,11 +45,84 @@ module MiniProxy
     end
   end
 
-  class ContentHandler
+  class ProxyServer < WEBrick::HTTPProxyServer
     attr_accessor :requests
 
-    def initialize
+    def initialize(config = {}, default = WEBrick::Config::HTTP)
       @requests = []
+      super(config, default)
+    end
+
+    # @param [WEBrick::HTTPRequest] req
+    # @param [WEBrick::HTTPResponse] res
+    def service(req, res)
+      # if req.unparsed_uri.match?("theconversation-temp-test")
+      #   binding.pry
+      #   temp = super(req, res)
+      #   binding.pry
+      # end
+
+      # TODO: FAILING? WHAT TO RETURN?
+      if (request = @requests.detect { |request| request.match?(req) })
+        if req.request_method == "CONNECT"
+          ua = Thread.current[:WEBrickSocket]  # User-Agent
+          # res.status = WEBrick::HTTPStatus::RC_OK
+          # res.send_response(ua)
+          # res.body = "CONNECT HTTP/1.0" + WEBrick::CRLF + WEBrick::CRLF
+          #
+          # # Should clear request-line not to send the response twice.
+          # # see: HTTPServer#run
+          # req.parse(NullReader) rescue nil
+          #
+          # # binding.pry
+          #
+
+
+          # do_CONNECT(req, res)
+          # "HTTP/1.1 200 Connection established\r\n\r\n"
+          # binding.pry
+          #
+
+          # binding.pry
+
+          temp = <<-STRING
+GET /sockjs-node/936/rnubhg5k/websocket HTTP/1.1
+Host: localhost:8080
+Connection: Upgrade
+Pragma: no-cache
+Cache-Control: no-cache
+Upgrade: websocket
+Origin: http://127.0.0.1:56091
+Sec-WebSocket-Version: 13
+User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36
+Accept-Encoding: gzip, deflate, br
+Accept-Language: en-US,en;q=0.9
+Sec-WebSocket-Key: RAR+YMI1nh9EbeIT/Vyf/w==
+Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits
+          STRING
+          ua.syswrite(temp)
+          res.status = 200
+          res.send_response(ua)
+
+          return
+        end
+
+        response = request.response
+
+        res.status = response.code
+        response.headers.each { |key, value| res[key] = value }
+        res.body = response.body
+
+        return
+      end
+
+      # Default response: 400 (Bad Request)
+      unless req.unparsed_uri.match?(MiniProxy::ALLOWED_HOSTS)
+        res.status = 400
+        return
+      end
+
+      super(req, res)
     end
 
     def stack_request(method:, url:, response:)
@@ -61,60 +135,27 @@ module MiniProxy
     def empty_request_stack
       @requests = []
     end
-
-    def call(req, res)
-      if (request = @requests.detect { |request| request.match?(req) })
-        response = request.response
-
-        res.status = response.code
-        response.headers.each { |key, value| res[key] = value }
-        res.body = response.body
-      end
-    end
-  end
-
-  class ProxyServer < WEBrick::HTTPProxyServer
-    # @param [WEBrick::HTTPRequest] req
-    # @param [WEBrick::HTTPResponse] res
-    def service(req, res)
-      # Default response: 400 (Bad Request)
-      unless req.unparsed_uri.match?(MiniProxy::ALLOWED_HOSTS)
-        res.status = 400
-        return ""
-      end
-
-      super(req, res)
-    end
   end
 
   class Server
-    attr_reader :handler
+    include Singleton
 
-    private_class_method :new
-
-    def self.start
-      @server ||= new
-    end
+    attr_reader :proxy
 
     def self.stub_request(method:, url:, response: {})
-      @server.handler.stack_request(method: method, url: url, response: response)
+      instance.proxy.stack_request(method: method, url: url, response: response)
     end
 
     def self.reset
-      @server.handler.empty_request_stack
+      instance.proxy.empty_request_stack
     end
 
     def initialize
-      @handler = MiniProxy::ContentHandler.new
-      @thread = Thread.new do
-        proxy = MiniProxy::ProxyServer.new(Port: MiniProxy::SERVER_PORT, ProxyContentHandler: @handler)
-        proxy.start
-      end
+      @proxy = MiniProxy::ProxyServer.new(Port: MiniProxy::SERVER_PORT)
+      @thread = Thread.new { @proxy.start }
     end
   end
 end
-
-MiniProxy::Server.start
 
 RSpec.configure do |config|
   config.after { puts MiniProxy::Server.reset }
