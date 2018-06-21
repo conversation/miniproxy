03b859e1b69be9c4e2785729568f4ac8b37df452
nickbrowne <nickbrowne@users.noreply.github.com>
2018-06-21 17:41:13 +1000

    Spec miniproxy/stub/response

--- spec/lib/miniproxy/stub/response_spec.rb
+++ spec/lib/miniproxy/stub/response_spec.rb
@@ -1,1 +1,47 @@
 require "miniproxy/stub/response"
+
+RSpec.describe MiniProxy::Stub::Response do
+  describe "#body" do
+    context "@body is set" do
+      let(:response) { MiniProxy::Stub::Response.new(headers: nil, body: "yolo") }
+
+      it "returns body" do
+        expect(response.body).to eql "yolo"
+      end
+    end
+
+    context "@body is not set" do
+      let(:response) { MiniProxy::Stub::Response.new(headers: nil, body: nil) }
+
+      it "returns an empty string" do
+        expect(response.body).to eql ""
+      end
+    end
+  end
+
+  describe "#code" do
+    let(:response) { MiniProxy::Stub::Response.new(headers: nil, body: nil) }
+
+    it "returns 200" do
+      expect(response.code).to eql 200
+    end
+  end
+
+  describe "#headers" do
+    context "@headers is set" do
+      let(:response) { MiniProxy::Stub::Response.new(headers: { hello: "world" }, body: nil) }
+
+      it "returns headers" do
+        expect(response.headers).to eql({ hello: "world" })
+      end
+    end
+
+    context "@headers is not set" do
+      let(:response) { MiniProxy::Stub::Response.new(headers: nil, body: nil) }
+
+      it "returns a default hash with Content-Type 'text/html'" do
+        expect(response.headers).to eql({ "Content-Type" => "text/html" })
+      end
+    end
+  end
+end
