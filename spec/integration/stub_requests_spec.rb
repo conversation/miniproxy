3a903801ac66c6018911aee5718aa57acb550cf3
nickbrowne <nickbrowne@users.noreply.github.com>
2018-06-21 15:39:57 +1000

    Extract capybara driver

--- spec/integration/stub_requests_spec.rb
+++ spec/integration/stub_requests_spec.rb
@@ -1,22 +1,7 @@
 require "capybara"
 require "miniproxy"
 require "selenium-webdriver"
-
-firefox_profile = Selenium::WebDriver::Firefox::Profile.new
-firefox_profile.assume_untrusted_certificate_issuer = true
-firefox_profile.proxy = Selenium::WebDriver::Proxy.new(
-  http: "#{MiniProxy::Server.host}:#{MiniProxy::Server.port}",
-  ssl: "#{MiniProxy::Server.host}:#{MiniProxy::Server.port}"
-)
-
-firefox_options = Selenium::WebDriver::Firefox::Options.new(profile: firefox_profile)
-firefox_options.headless!
-
-firefox_caps = Selenium::WebDriver::Remote::Capabilities.firefox(accept_insecure_certs: true)
-
-Capybara.register_driver :firefox do |app|
-  Capybara::Selenium::Driver.new(app, desired_capabilities: firefox_caps, options: firefox_options)
-end
+require "support/capybara_driver"
 
 RSpec.describe "miniproxy" do
   let(:session) { Capybara::Session.new(:firefox) }
---
+++ spec/support/capybara_driver.rb
@@ -0,0 +1,15 @@
+firefox_profile = Selenium::WebDriver::Firefox::Profile.new
+firefox_profile.assume_untrusted_certificate_issuer = true
+firefox_profile.proxy = Selenium::WebDriver::Proxy.new(
+  http: "#{MiniProxy::Server.host}:#{MiniProxy::Server.port}",
+  ssl: "#{MiniProxy::Server.host}:#{MiniProxy::Server.port}"
+)
+
+firefox_options = Selenium::WebDriver::Firefox::Options.new(profile: firefox_profile)
+firefox_options.headless!
+
+firefox_caps = Selenium::WebDriver::Remote::Capabilities.firefox(accept_insecure_certs: true)
+
+Capybara.register_driver :firefox do |app|
+  Capybara::Selenium::Driver.new(app, desired_capabilities: firefox_caps, options: firefox_options)
+end
